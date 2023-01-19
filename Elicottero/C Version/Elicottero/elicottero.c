6a4ece61a21430b7bdcd6d77d89d5e7abc177cd7
MicheleMosca <michele.mosca026@gmail.com>
2023-01-19 19:42:47 +0100

    program finished

--- Elicottero/C Version/Elicottero/elicottero.c
+++ Elicottero/C Version/Elicottero/elicottero.c
@@ -12,6 +12,7 @@ struct definizione_gruppo{
 };
 
 typedef struct definizione_gruppo gruppo;
+typedef enum {false, true} Boolean;
 
 #define PASSEGGERO_NON_SERVITO -1
 #define PASSEGGERO_SERVITO -2
@@ -33,6 +34,8 @@ int NUM_POSTI;  /* variabile che indica il numero di posti che l'elicottero cont
 int NUM_THREADS_PASSEGGERI_SINGOLI; /* variabile che indica il numero di thread passeggeri singoli che deve essere generato */
 int NUM_THREADS_PASSEGGERI_GRUPPO;  /* variabile che indica il numero di thread passeggeri in gruppo che deve essere generato */
 
+int voli_eseguiti;  /* variabile contatore che serve a memorizzare il numero di voli che il pilota ha effettuato */
+
 int mia_random(int MAX)
 {
     int casuale;    /* variabile che conterra' il numero casuale */
@@ -43,7 +46,7 @@ int mia_random(int MAX)
     return casuale;
 }
 
-void PRENOTA(int id, int num_persone)
+void PRENOTA(int id, int num_persone, Boolean *voli_finiti)
 {
     pthread_mutex_lock(&mutex); /* simulazione dell'ingresso nella procedure entry di un monitor */
 
@@ -68,13 +71,23 @@ void PRENOTA(int id, int num_persone)
 
     while (passeggeri[id - 1] == PASSEGGERO_NON_SERVITO)
     {
+        if (voli_eseguiti == NUM_VOLI)  /* se i voli sono terminati torno a casa */
+        {
+            *voli_finiti = true; /* indico che i voli sono stati terminati, dunque i passeggeri tornano a casa */
+            pthread_mutex_unlock(&mutex);   /* simulazione del termine di una procedure entry di un monitor */
+            return;
+        }
+
         /* mi metto in attesa che il pilota mi/ci faccia salire sull'elicottero */
         pthread_cond_wait(&attesa_imbarco, &mutex);
     }
 
-    printf("AUTO-[Thread%d e identificatore %lu] il pilota con id %d mi/ci ha fatto salire sull'elicottero\n", id, pthread_self(), passeggeri[id - 1]);
+    if (num_persone == 1)
+        printf("PASSEGGERO_SINGOLO-[Thread%d e identificatore %lu] il pilota con id %d mi ha fatto salire sull'elicottero\n", id, pthread_self(), passeggeri[id - 1]);
+    else
+        printf("PASSEGGERI_GRUPPO-[Thread%d e identificatore %lu] il pilota con id %d ci ha fatto salire sull'elicottero\n", id, pthread_self(), passeggeri[id - 1]);
 
-    while(passeggeri[id - -1] == PASSEGGERO_SERVITO)
+    while(passeggeri[id - 1] != PASSEGGERO_SERVITO)
     {
         /* l'elicottero e' appena partito, attendo il termine del volo */
         pthread_cond_wait(&attesa_termine, &mutex);
@@ -83,18 +96,115 @@ void PRENOTA(int id, int num_persone)
     pthread_mutex_unlock(&mutex);   /* simulazione del termine di una procedure entry di un monitor */
 }
 
-void IMBARCO(int id)
+void IMBARCO(int id, int *num_posti_disponibili, int **id_selezionati, int *contatore_selezionati)
 {
     pthread_mutex_lock(&mutex); /* simulazione dell'ingresso nella procedure entry di un monitor */
 
+    int i;  /* variabile contatore usata per la selezione del gruppo di passeggeri */
+    int j;  /* variabile contatore usata per spostare i passeggeri in gruppo/singoli nella coda */
+    Boolean trovato;    /* variabile booleana che indica se e' stato trovato un gruppo di passeggeri che può riempire i posti disponibili in elicottero */
 
+    /* controllo se ci sono gruppi di passeggeri in attesa */
+    if (contatore_passeggeri_gruppo > 0)
+    {
+        trovato = true; /* imposto 'trovato' a true per entrare nel ciclo */
+        while (trovato == true) /* ripeto la ricerca fin quando non trovo piu' gruppi di passeggeri in grado di riempire i posti rimanenti */
+        {
+            /* imposto 'trovato' a false cose' che se la ricerca non ha avuto esito positivo usciro' dal ciclo */
+            trovato = false;
+
+            for (i = 0; i < contatore_passeggeri_gruppo; i++)
+            {
+                /* occupo i posti disponibili inserendo prima i passeggeri in gruppo */
+                if (*num_posti_disponibili - coda_passeggeri_gruppo[i].num_persone >= 0)
+                {
+                    /* faccio salire il gruppo di passeggeri in elicottero, inserendo nel loro stato il mio id pilota e inserendo il loro id nell'array id_selezionati */
+                    passeggeri[coda_passeggeri_gruppo[i].id - 1] = id;
+                    (*id_selezionati)[*contatore_selezionati] = coda_passeggeri_gruppo[i].id;
+                    printf("PILOTA-[Thread%d e identificatore %lu] faccio salire il gruppo di passeggeri con id %d composto da %d persone. Posti ancora disponibili: %d\n", id, pthread_self(), coda_passeggeri_gruppo[i].id, coda_passeggeri_gruppo[i].num_persone, *num_posti_disponibili - coda_passeggeri_gruppo[i].num_persone);
+
+                    /* aumento di uno il numero dei passeggeri/gruppi di passeggeri selezionati per salire sull'elicottero */
+                    (*contatore_selezionati)++;
+
+                    /* diminuisco il numero di posti disponibili */
+                    *num_posti_disponibili -= coda_passeggeri_gruppo[i].num_persone;
+
+                    /* rimuovo dalla coda il gruppo di passeggeri selezionato */
+                    for (j = i; j < contatore_passeggeri_gruppo-1; j++)  /* sposto tutti gli altri gruppi di passeggeri di una posizione in coda */
+                        coda_passeggeri_gruppo[j] = coda_passeggeri_gruppo[j+1];
+
+                    /* resetto l'ultima posizione della coda */
+                    (coda_passeggeri_gruppo[j]).id = -1;
+                    (coda_passeggeri_gruppo[j]).num_persone = 0;
+
+                    /* diminuisco di uno il numero di gruppi di passeggeri in coda */
+                    contatore_passeggeri_gruppo--;
+
+                    /* dato che ho trovato un gruppo che riempie i posti rimanenti imposto 'trovato' a true */
+                    trovato = true;
+
+                    /* termino la ricerca del gruppo di passeggeri */
+                    break;
+                }
+            }
+        }
+    }
+
+    while (contatore_passeggeri_singoli > 0 && *num_posti_disponibili > 0)  /* se rimangono ancora posti in elicottero e ci sono passeggeri singoli in coda, occupo i posti rimanenti facendoli salire */
+    {
+        /* faccio salire il passeggero singolo in elicottero, inserendo nel suo stato il mio id pilota e inserendo il suo id nell'array id_selezionati */
+        passeggeri[coda_passeggeri_singoli[0] - 1] = id;
+        (*id_selezionati)[*contatore_selezionati] = coda_passeggeri_singoli[0];
+        printf("PILOTA-[Thread%d e identificatore %lu] faccio salire il passeggero singolo con id %d. Posti ancora disponibili: %d\n", id, pthread_self(), coda_passeggeri_singoli[0], *num_posti_disponibili);
+
+        /* aumento di uno il numero dei passeggeri/gruppi di passeggeri selezionati per salire sull'elicottero */
+        (*contatore_selezionati)++;
+
+        /* diminuisco il numero di posti disponibili */
+        (*num_posti_disponibili)--;
+
+        /* rimuovo dalla coda il passeggero selezionato */
+        for (j = 0; j < contatore_passeggeri_singoli - 1; j++)  /* sposto tutti gli altri passeggeri di una posizione in coda */
+            coda_passeggeri_singoli[j] = coda_passeggeri_singoli[j+1];
+
+        /* resetto l'ultima posizione della coda */
+        coda_passeggeri_singoli[j] = -1;
+
+        /* diminuisco di uno il numero dei passeggeri singoli in coda */
+        contatore_passeggeri_singoli--;
+    }
+
+    printf("PILOTA-[Thread%d e identificatore %lu] elicottero in partenza, numero passeggeri: %d\n", id, pthread_self(), NUM_POSTI - *num_posti_disponibili);
+
+    /* notifico i passeggeri che l'elicottero e' in partenza */
+    pthread_cond_broadcast(&attesa_imbarco);
 
     pthread_mutex_unlock(&mutex);   /* simulazione del termine di una procedure entry di un monitor */
 }
 
-void VOLO_TERMINATO(int id)
+void VOLO_TERMINATO(int id, int *num_posti_disponibili, int **id_selezionati, int *contatore_selezionati)
 {
     pthread_mutex_lock(&mutex); /* simulazione dell'ingresso nella procedure entry di un monitor */
+
+    printf("PILOTA-[Thread%d e identificatore %lu] volo %d-esimo terminato, faccio scendere i passeggeri\n", id, pthread_self(), voli_eseguiti);
+
+    while (*contatore_selezionati > 0)
+    {
+        /* imposto lo stato dei passeggeri in PASSEGGERO_SERVITO (-2) per indicare che il loro volo e' terminato */
+        passeggeri[(*id_selezionati)[*contatore_selezionati -1] - 1] = PASSEGGERO_SERVITO;
+
+        /* libero il posto in elicottero */
+        (*id_selezionati)[*contatore_selezionati - 1] = -1;
+
+        /* diminuisco il contatore dell'array id_selezionati */
+        (*contatore_selezionati)--;
+    }
+
+    /* ripristino il numero di posti disponibili in elicottero pari a NUM_POSTI */
+    *num_posti_disponibili = NUM_POSTI;
+
+    /* notifico i passeggeri che il volo e' terminato */
+    pthread_cond_broadcast(&attesa_termine);
 
     pthread_mutex_unlock(&mutex);   /* simulazione del termine di una procedure entry di un monitor */
 }
@@ -138,6 +248,7 @@ void *eseguiPasseggeroSingolo(void *id)
 {
     int *pi = (int *) id;
     int *ptr;
+    Boolean voli_finiti; /* variabile booleana che indica se sono terminati i voli e pertanto il passeggero e' costretto a tornare a casa */
 
     ptr = (int *) malloc(sizeof(int));
     if (ptr == NULL)
@@ -146,12 +257,18 @@ void *eseguiPasseggeroSingolo(void *id)
         exit(-1);
     }
 
+    /* inizializzo la variabile voli_terminati a false dato che al momento non so se sono gia' finiti */
+    voli_finiti = false;
+
     printf("PASSEGGERO_SINGOLO-[Thread%d e identificatore %lu] STO ARRIVANDO\n", *pi, pthread_self());
 
     /* effettuo la prenotazione per un passeggero singolo */
-    PRENOTA(*pi, 1);
+    PRENOTA(*pi, 1, &voli_finiti);
 
-    printf("PASSEGGERO_SINGOLO-[Thread%d e identificatore %lu] volo terminato, VADO A CASA\n", *pi, pthread_self());
+    if (voli_finiti)
+        printf("PASSEGGERO_SINGOLO-[Thread%d e identificatore %lu] il pilota con ha terminato i voli per oggi, TORNO A CASA\n", *pi, pthread_self());
+    else
+        printf("PASSEGGERO_SINGOLO-[Thread%d e identificatore %lu] volo terminato, VADO A CASA\n", *pi, pthread_self());
 
     /* pthread vuole tornare al padre il valore del suo id */
     *ptr = *pi;
@@ -164,6 +281,7 @@ void *eseguiPasseggeriGruppo(void *id)
     int *pi = (int *) id;
     int *ptr;
     int num_persone;    /* variabile che contiene il numero di persone che compone il gruppo, generato in modo random */
+    Boolean voli_finiti; /* variabile booleana che indica se sono terminati i voli e pertanto il gruppo di passeggeri e' costretto a tornare a casa */
 
     ptr = (int *) malloc(sizeof(int));
     if (ptr == NULL)
@@ -171,6 +289,9 @@ void *eseguiPasseggeriGruppo(void *id)
         perror("Errore: Problemi con l'allocazione di ptr\n");
         exit(-1);
     }
+
+    /* inizializzo la variabile voli_terminati a false dato che al momento non sappiamo se sono gia' finiti */
+    voli_finiti = false;
 
     printf("PASSEGGERI_GRUPPO-[Thread%d e identificatore %lu] STIAMO ARRIVANDO\n", *pi, pthread_self());
 
@@ -182,9 +303,12 @@ void *eseguiPasseggeriGruppo(void *id)
         num_persone++;
 
     /* effettuo la prenotazione per un gruppo di passeggeri */
-    PRENOTA(*pi, num_persone);
+    PRENOTA(*pi, num_persone, &voli_finiti);
 
-    printf("PASSEGGERI_GRUPPO-[Thread%d e identificatore %lu] volo terminato, ANDIAMO A CASA\n", *pi, pthread_self());
+    if (voli_finiti)
+        printf("PASSEGGERI_GRUPPO-[Thread%d e identificatore %lu] il pilota ha terminato i voli per oggi, TORNIAMO A CASA\n", *pi, pthread_self());
+    else
+        printf("PASSEGGERI_GRUPPO-[Thread%d e identificatore %lu] volo terminato, ANDIAMO A CASA\n", *pi, pthread_self());
 
     /* pthread vuole tornare al padre il valore del suo id */
     *ptr = *pi;
@@ -196,7 +320,9 @@ void *eseguiPilota(void *id)
 {
     int *pi = (int *) id;
     int *ptr;
-    int voli_eseguiti;  /* variabile contatore che serve a memorizzare il numero di voli che il pilota ha effettuato */
+    int num_posti_disponibili;  /* variabile che contiene il numero di posti attualmente disponibili per il volo corrente */
+    int *id_selezionati; /* array contenente gli id dei passeggeri/gruppi di passeggeri che sono stati selezionati per salire sull'elicottero */
+    int contatore_selezionati; /* variabile contatore per l'array id_selezionati */
 
     ptr = (int *) malloc(sizeof(int));
     if (ptr == NULL)
@@ -205,7 +331,25 @@ void *eseguiPilota(void *id)
         exit(-1);
     }
 
+    /* definisco l'array contenente l'id dei passeggeri selezionati per salire sull'elicottero */
+    id_selezionati = (int *)  malloc(NUM_POSTI * sizeof(int));
+    if (id_selezionati == NULL)
+    {
+        perror("Errore: Problemi con l'allocazione di id_selezionati\n");
+        exit(-1);
+    }
+
+    /* inizializzo l'array id_selezionati con tutti gli elementi a -1 (posto libero), nessun passeggero selezionato */
+    for(contatore_selezionati = 0; contatore_selezionati < NUM_POSTI; contatore_selezionati++)
+        id_selezionati[contatore_selezionati] = -1;
+
+    /* azzero il contatore */
+    contatore_selezionati = 0;
+
     printf("PILOTA-[Thread%d e identificatore %lu] STO ARRIVANDO\n", *pi, pthread_self());
+
+    /* inizialmente il numero di posti disponibili sara' pari a NUM_POSTI */
+    num_posti_disponibili = NUM_POSTI;
 
     /* eseguo i voli di questa giornata */
     for (voli_eseguiti = 0; voli_eseguiti < NUM_VOLI; voli_eseguiti++)
@@ -214,16 +358,28 @@ void *eseguiPilota(void *id)
         sleep(2);
 
         /* faccio salire i passeggeri sull'elicottero */
-        IMBARCO(*pi);
+        printf("PILOTA-[Thread%d e identificatore %lu] e' arrivata l'ora di partire, effettuo l'imbarco per il %d-esimo volo\n", *pi, pthread_self(), voli_eseguiti);
+        IMBARCO(*pi, &num_posti_disponibili, &id_selezionati, &contatore_selezionati);
 
         /* simulo il volo mediante una sleep di 4 secondi */
         sleep(4);
 
         /* termino il volo e faccio scendere i passeggeri */
-        VOLO_TERMINATO(*pi);
+        VOLO_TERMINATO(*pi, &num_posti_disponibili, &id_selezionati, &contatore_selezionati);
     }
 
-    printf("PILOTA-[Thread%d e identificatore %lu] voli terminati, VADO A CASA\n", *pi, pthread_self());
+    if (contatore_passeggeri_singoli > 0 || contatore_passeggeri_gruppo > 0)    /* se i voli sono terminati ma ci sono ancora passeggeri in attesa, li sblocco */
+    {
+        printf("PILOTA-[Thread%d e identificatore %lu] i voli sono finiti ma ci sono ancora passeggeri in coda, li notifico e VADO A CASA\n", *pi, pthread_self());
+
+        /* notifico i passeggeri in attesa */
+        pthread_cond_broadcast(&attesa_imbarco);
+    }
+    else
+        printf("PILOTA-[Thread%d e identificatore %lu] voli terminati, VADO A CASA\n", *pi, pthread_self());
+
+    /* dealloco la memoria per l'array id_selezionati non piu' utilizzato */
+    free(id_selezionati);
 
     /* pthread vuole tornare al padre il valore del suo id */
     *ptr = *pi;
@@ -261,7 +417,7 @@ int main(int argc, char **argv)
     NUM_POSTI = atoi(argv[2]);
     if (NUM_POSTI < 2)   /* Controllo che il numero di POSTI che l'elicottero contiene sia maggiore o uguale a due */
     {
-        sprintf(error, "Errore: Numero di POSTI insufficienti per l'avvio del programma\n");
+        sprintf(error, "Errore: Numero di POSTI insufficienti per l'avvio del programma, minimo 2\n");
         perror(error);
         exit(3);
     }
@@ -397,6 +553,9 @@ int main(int argc, char **argv)
             }
             printf("SONO IL MAIN e ho creato il Pthread PASSEGGERI_GRUPPO %i-esimo con id=%lu\n", i, thread[i]);
         }
+
+        /* effettuo una sleep di 1 secondo tra la creazione di un thread e un altro per non riempire sin da subito le code, simulando cosi' un arrivo dei passeggeri in orari differenti */
+        sleep(1);
     }
 
     /* Aspetto il termine di tutti threads generati */
